81b05b5a095edaa71d90530d13cd492f7b7ab795
Elizabeth Moore <ekm18c@fsu.edu>
2023-07-17 19:16:19 +0000

    Update team.js

--- static/team.js
+++ static/team.js
@@ -5,10 +5,24 @@ const fishContainer = document.querySelector('.fish-container');
 const banner = document.querySelector('.banner');
 const bannerText = document.querySelector('.banner h2');
 
-fishContainer.addEventListener('mouseover', () => {
-  banner.classList.add('show');
+const containerRect = fishContainer.getBoundingClientRect();
+const bannerRect = banner.getBoundingClientRect();
+
+fishContainer.addEventListener('mousemove', (event) => {
+  const mouseX = event.clientX - containerRect.left;
+  const mouseY = event.clientY - containerRect.top;
+
+  const bannerX = mouseX - bannerRect.width / 2;
+  const bannerY = mouseY - bannerRect.height / 2;
+
+  banner.style.transform = `translate(${bannerX}px, ${bannerY}px)`;
 });
 
-fishContainer.addEventListener('mouseout', () => {
+fishContainer.addEventListener('mouseleave', () => {
+  banner.style.transform = 'translate(-50%, -50%)';
   banner.classList.remove('show');
+});
+
+fishContainer.addEventListener('mouseenter', () => {
+  banner.classList.add('show');
 });
