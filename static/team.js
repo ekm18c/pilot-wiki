f298dde45697d1a3e8911f5fedafb4b57f612f85
Elizabeth Moore <ekm18c@fsu.edu>
2023-07-17 19:41:21 +0000

    Update team.js

--- static/team.js
+++ static/team.js
@@ -1,28 +1,17 @@
 function flipCard(card) {
   card.classList.toggle("flipped");
 }
+function flipCard(card) {
+  card.classList.toggle("flipped");
+}
 const fishContainer = document.querySelector('.fish-container');
 const banner = document.querySelector('.banner');
 const bannerText = document.querySelector('.banner h2');
 
-const containerRect = fishContainer.getBoundingClientRect();
-const bannerRect = banner.getBoundingClientRect();
-
-fishContainer.addEventListener('mousemove', (event) => {
-  const mouseX = event.clientX - containerRect.left;
-  const mouseY = event.clientY - containerRect.top;
-
-  const bannerX = mouseX - bannerRect.width / 2;
-  const bannerY = mouseY - bannerRect.height / 2;
-
-  banner.style.transform = `translate(${bannerX}px, ${bannerY}px)`;
+fishContainer.addEventListener('mouseover', () => {
+  bannerText.style.opacity = 1; // Show the banner text
 });
 
-fishContainer.addEventListener('mouseleave', () => {
-  banner.style.transform = 'translate(-50%, -50%)';
-  banner.classList.remove('show');
-});
-
-fishContainer.addEventListener('mouseenter', () => {
-  banner.classList.add('show');
+fishContainer.addEventListener('mouseout', () => {
+  bannerText.style.opacity = 0; // Hide the banner text
 });
