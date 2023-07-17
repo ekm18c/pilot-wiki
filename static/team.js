8554d70adbb43b34b58803fc5cbd8200758c310e
Elizabeth Moore <ekm18c@fsu.edu>
2023-07-17 16:31:39 +0000

    Update team.js

--- static/team.js
+++ static/team.js
@@ -1,3 +1,13 @@
 function flipCard(card) {
     card.classList.toggle("flipped");
 }
+const container = document.querySelector('.container');
+const overlay = document.querySelector('.banner');
+
+container.addEventListener('mouseover', () => {
+  overlay.style.opacity = 1;
+});
+
+container.addEventListener('mouseout', () => {
+  overlay.style.opacity = 0;
+});
