75132b429c729aed5630615343e80e3d1e369480
Elizabeth Moore <ekm18c@fsu.edu>
2023-07-17 23:31:01 +0000

    Update team.js

--- static/team.js
+++ static/team.js
@@ -1,3 +1,12 @@
 function flipCard(card) {
   card.classList.toggle("flipped");
 }
+
+const fishContainer = document.querySelector('.fish-container');
+const fadingEffect = document.querySelector('.fading-effect');
+
+fishContainer.addEventListener('animationend', revealText);
+
+function revealText() {
+  fadingEffect.classList.add('fading-effect-reveal');
+}
